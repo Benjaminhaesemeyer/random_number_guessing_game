1d4eacad36d8b381c879f9cc571b4a5eaa93e266
Benjaminhaesemeyer <benjaminhaesemeyer@gmail.com>
2017-06-29 16:10:11 -0500

    added for loop and randomNum function

--- server/server.js
+++ server/server.js
@@ -4,6 +4,27 @@ var path = require('path');
 var port = 5000;
 
 
+//random number function
+function randomNum(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+
+
+
+var array = [];
+var answer = randomNum();
+//added for loop
+for(i = 0; i > array.length; i += 1){
+  if (array[i] < answer){
+    return "Too Low"
+  } else if (array[i] > answer){
+    return "Too High"
+  } else {
+    return "Correct"
+  }
+}
+
 app.get('/*', function(req, res) {
   var file = req.params[0] || '/views/index.html';
   res.sendFile(path.join(__dirname, '/public/', file));
